edf86bce566cf0e6b4dd4ae56d5b746daed79c18
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-02 18:32:15 -0500

    add event listener to button element

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 const inputElement = document.getElementById('inp')
-console.log(inputElement)
+
 
 const buttonElement = document.getElementById('submit-button')
-console.log(buttonElement)
+
+
+const ulElement = document.getElementById('todo-list')
+
+buttonElement.addEventListener('click', function(event){
+  // create a new li element
+  const newLi = document.createElement('li')
+  console.log(newLi)
+  // set the text of the new li element to the text the user entered in input
+
+  // reset the input text to an empty string
+
+  // add the new li element to the page inside the ul element 
+})
