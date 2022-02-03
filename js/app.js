be029df8bcd73e4993fcc798528a54561fe7a39d
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-02 19:15:06 -0500

    prevent empty items from being added to list

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
 const inputElement = document.getElementById('inp')
 
-
 const buttonElement = document.getElementById('submit-button')
 
 
 const ulElement = document.getElementById('todo-list')
 
+
 buttonElement.addEventListener('click', function(event){
-  // create a new li element
   const newLi = document.createElement('li')
+  // create a new li element
+  
+  newLi.textContent = inputElement.value
   console.log(newLi)
   // set the text of the new li element to the text the user entered in input
 
-  // reset the input text to an empty string
-
-  // add the new li element to the page inside the ul element 
+if(inputElement.value !== ''){
+  // if the user's input is not empty
+    // prevents an empty bullet point being created
+  ulElement.appendChild(newLi)
+  // add the new li element to the page inside the ul element
+  inputElement.value = ''
+   // reset the input text to an empty string
+}
+   
 })
